9b405109e3c4942e189ccb2f1a092bc959ce6656
rahmathidaya13 <rahmathidaya1305@gmail.com>
2024-08-11 23:22:34 +0700

    hide comment

--- script.js
+++ script.js
@@ -8,19 +8,17 @@ setInterval(function () {
   document.getElementById("time").innerText = moment().format("H : mm");
 }, 1000);
 
-/* fungsi ini untuk memformat angka dengan menambahkan 
-titik setiap 3 digit untuk memudahkan pembacaan angka */
+
 function formatNumber(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
-// atur angka didalam fungsi
+
 function number(number) {
   if (number === "." && currentOperand.includes(".")) return;
   currentOperand = currentOperand.toString() + number.toString();
   updateDisplay();
 }
 
-// fungsi ini untuk menghapus jumlah angka satu per satu
 function backspace() {
   currentOperand = currentOperand.toString().slice(0, -1);
   if (currentOperand === "") currentOperand = "";
@@ -28,14 +26,12 @@ function backspace() {
   updateDisplay();
 }
 
-// fungsi ini untuk menampilkan angka yang dibuat dan menampilkan jumlah angka yang dhitung
 function updateDisplay() {
   document.getElementById("show").innerText = formatNumber(
     currentOperand || "0"
   );
 }
 
-// fungsi ini untuk menghapus seluruh angka pada display
 function clearAll() {
   currentOperand = "";
   previousOperand = "";
@@ -44,7 +40,6 @@ function clearAll() {
   updateDisplay();
 }
 
-// fungsi ini untuk memberikan operator berdasarkan operato yang ingin ditambahkan
 function operation(operand) {
   if (currentOperand === "" && previousOperand !== "") {
     operator = operand;
@@ -52,16 +47,13 @@ function operation(operand) {
     return;
   }
   if (currentOperand === "") return;
-  if (previousOperand !== "") {
-    calculate();
-  }
+  if (previousOperand !== "")calculate();
   operator = operand;
   previousOperand = currentOperand;
   currentOperand = "";
   displayOperator.innerText = operand;
 }
 
-// fungsi ini untuk melakukan perhitungan menggunakan operator yang dipilih
 function calculate() {
   let result;
   let prev = parseFloat(previousOperand);
